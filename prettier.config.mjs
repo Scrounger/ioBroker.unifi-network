e501bab226774dac1bcf37eeb10f591d05f95283
Scrounger <scrounger@gmx.net>
2025-09-21 20:08:13 +0200

    house keeping

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+// iobroker prettier configuration file
+import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
+
+export default {
+    ...prettierConfig,
+    printWidth: 600,
+    // uncomment next line if you prefer double quotes
+    // singleQuote: false,
+}
